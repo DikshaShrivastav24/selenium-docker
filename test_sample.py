1cf7fbb2828cea8a34cf2b91e627964eff3ef3ac
Diksha Shrivastav <DikshaShrivastav2424@gmail.com>
2025-05-02 23:57:09 +0530

    Initial commit with Dockerized Selenium tests

---
+++ test_sample.py
@@ -0,0 +1,8 @@
+# test_sample.py
+from selenium import webdriver
+
+def test_google():
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.com")
+    assert "Google" in driver.title
+    driver.quit()
